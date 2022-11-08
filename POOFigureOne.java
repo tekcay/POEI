f8a01e15ce47731c686ab3a643f269630b4eb6f9
tekcay <pierreblc@hotmail.fr>
2022-11-08 10:39:24 +0100

    added Carre and Rectangle classes

--- POOFigureOne.java
+++ POOFigureOne.java
@@ -1,38 +1,57 @@
-import java.util.*;
-import java.util.ArrayList.*;
-
-
-class Point
-{
+class Point {
     protected Integer x, y;
 
-    public Point( int x, int y )
-    {
+    public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    public void affiche()
-    {
-        System.out.println( String.format( "Point : (%d, %d)", this.x, this.y ) );
+    public void affiche() {
+        System.out.printf("Point : (%d, %d)%n", this.x, this.y);
     }
 }
 
-class Cercle extends Point
-{
+class Cercle extends Point {
     int rayon;
 
-    public Cercle( int x, int y, int r )
-    {
+    public Cercle(int x, int y, int rayon) {
         super(x, y);
-        this.rayon = r;
+        this.rayon = rayon;
     }
 
-    public void affiche()
-    {
-        System.out.println( String.format( "Cercle : (%d, %d) rayon=%d", this.x, this.y, this.rayon ) );
+    public void affiche() {
+        System.out.printf("Cercle : (%d, %d) rayon=%d%n", this.x, this.y, this.rayon);
     }
 }
+
+class Carre extends Point {
+    int cote;
+
+    public Carre(int x, int y, int cote) {
+        super(x, y);
+        this.cote = cote;
+    }
+
+    @Override
+    public void affiche() {
+        System.out.printf("Carre : (%d, %d) cote=%d%n", this.x, this.y, this.cote);
+    }
+}
+
+class Rectangle extends Carre {
+    int largeur;
+
+    public Rectangle(int x, int y, int cote, int largeur) {
+        super(x, y, cote);
+        this.largeur = cote;
+    }
+
+    @Override
+    public void affiche() {
+        System.out.printf("Rectangle : (%d, %d) long=%d, lar=%d%n", this.x, this.y, this.cote, this.largeur);
+    }
+}
+
 
 
 
@@ -40,26 +59,22 @@ class Cercle extends Point
 class  POOFigureOne {
     // Xavier
 
-    public static void main(String[] args) 
-    {
+    public static void main(String[] args)  {
 
-        Point p1 = new Point( 5, 6 );
+        Point p1 = new Point(5, 6);
         p1.affiche();
         // (5, 6)
 
-        Cercle c1 = new Cercle( 11, 61, 45 );
+        Cercle c1 = new Cercle(11, 61, 45);
         c1.affiche();
         // (5, 6)
 
-
-        /*
-        Carre k1 = new Carre( 18, -3, 15 );
+        Carre k1 = new Carre(18, -3, 15);
         k1.affiche();
         // Carre : (5, 6) cote=15
-        Rectangle r1 = new Rectangle( 3, 19, 99, 18 );
+        Rectangle r1 = new Rectangle(3, 19, 99, 18);
         r1.affiche();
         // Rectangle : (5, 6) long=99 lar=18
 
-         */
     }
 }

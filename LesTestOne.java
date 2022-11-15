ddd3e00e398a37a167d451e8b3155ffa2abc638a
tekcay <pierreblc@hotmail.fr>
2022-11-15 14:27:30 +0100

    formatting and added comments

--- LesTestOne.java
+++ LesTestOne.java
@@ -1,25 +1,23 @@
-// Bourget
-
-class Atester 
-{
-    static public Integer calcul1( int var )
-    {
+class Atester {
+    static public Integer calcul1(int var) {
         return 5 * var;
     }
 
 
-    static public String presentation( String n )
-    {
+    static public String presentation(String n) {
+        //If input string is empty, returns empty
         if (n.equals("")) return "";
+
+        //takes the first character of the String
         String firstCharacter = Character.toString(n.charAt(0));
 
+        //If the first character can't be transformed to UpperCase,
+        //e.g if it is a number, catches the exception and returns the input String
         try {
             return firstCharacter.toUpperCase() + n.substring(1).toLowerCase();
-
         } catch (Exception e) {
             return n;
         }
-
     }
 
 
@@ -28,37 +26,37 @@ class Atester
 
 
 public class LesTestOne {
-    public static void main( String[] args )
-    {
-        if ( Atester.calcul1( 3 ) == 15 )
-            System.out.println( "OK" );
+    public static void main(String[] args) {
 
-        if ( Atester.calcul1( 0 ) == 0 )
-            System.out.println( "OK" );
+        if (Atester.calcul1(3) == 15)
+            System.out.println("OK");
 
-        if ( Atester.calcul1( 1 ) == 5 )
+        if (Atester.calcul1(0) == 0)
+            System.out.println("OK");
+
+        if (Atester.calcul1(1) == 5)
             System.out.println("OK");
 
         /* ******************************* */
 
-        if ( Atester.presentation( "toto" ).equals( "Toto") )
-            System.out.println( "OK" );
+        if (Atester.presentation("toto").equals("Toto"))
+            System.out.println("OK");
         else
-            System.out.println( "KO" );
+            System.out.println("KO");
  
-        if ( Atester.presentation( "TOTO" ).equals( "Toto") )
-            System.out.println( "OK" );
+        if (Atester.presentation("TOTO").equals("Toto"))
+            System.out.println("OK");
         else
-            System.out.println( "KO" );
+            System.out.println("KO");
  
-        if ( Atester.presentation( "" ).equals( "") )
-            System.out.println( "OK" );
+        if (Atester.presentation("").equals(""))
+            System.out.println("OK");
         else
-            System.out.println( "KO" );
+            System.out.println("KO");
  
-        if ( Atester.presentation( "1234567890" ).equals( "1234567890") )
-            System.out.println( "OK" );
+        if (Atester.presentation("1234567890").equals("1234567890"))
+            System.out.println("OK");
         else
-            System.out.println( "KO" );
+            System.out.println("KO");
     }    
 }
